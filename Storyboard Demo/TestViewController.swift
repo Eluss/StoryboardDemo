f6c60c8802903b9efd77690d3c3f3b839140e3fc
Appcamp <appcamp@systemss-MacBook-Pro-2.local>
2015-07-18 12:00:43 +0200

    Custom view controller

--- Storyboard Demo/TestViewController.swift
+++ Storyboard Demo/TestViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TestViewController.swift
 //  Storyboard Demo
 //
 //  Created by Appcamp on 18/07/15.
@@ -8,21 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TestViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        println("custom view controller")
         //test
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-   
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
